e3c97169162c7ca43f5fa8d91db058e375be2eb7
edancabrera <ecabrerait@gmail.com>
2025-12-09 13:19:11 -0700

    Estilado del tab de dashboard

--- src/app/dashboard/_layout.jsx
+++ src/app/dashboard/_layout.jsx
@@ -3,7 +3,21 @@ import { Tabs } from 'expo-router'
 
 const dashbaordLayout = () => {
   return (
-    <Tabs>
+    <Tabs
+        screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: "#faa80f",
+            tabBarInactiveTintColor: "#fff",
+            tabBarStyle: {
+                backgroundColor: "#2596be",
+                paddingBottom: 8
+            },
+            tabBarLabelStyle: {
+                fontSize: 18,
+                fontWeight: 'bold'
+            }
+        }}
+    >
         <Tabs.Screen name = "mesas"/>
         <Tabs.Screen name = "articulos"/>
     </Tabs>
