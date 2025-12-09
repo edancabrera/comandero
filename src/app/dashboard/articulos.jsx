404b0bdd6cbcf2ce597af9d8f32963e182e4ed2b
edancabrera <ecabrerait@gmail.com>
2025-12-09 14:00:29 -0700

    uso de FlatList simular una tabla con los productos y sus precios

--- src/app/dashboard/articulos.jsx
+++ src/app/dashboard/articulos.jsx
@@ -1,7 +1,14 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Articulos = () => {
+  const productos = [
+    { id: "1", clave: "A001", nombre: "Coca-Cola 600ml", precio: 18 },
+    { id: "2", clave: "A002", nombre: "Pepsi 600ml", precio: 17 },
+    { id: "3", clave: "B055", nombre: "Agua Ciel 1L", precio: 14 },
+    { id: "4", clave: "M020", nombre: "Jugo Del Valle", precio: 22 },
+  ];
+
   return (
     <SafeAreaView style={styles.container}>
 
@@ -17,6 +24,19 @@ const Articulos = () => {
         <Text style={[styles.headerText, {flex:2}]}>Platillo/Bebida</Text>
         <Text style={[styles.headerText]}>Precio</Text>
       </View>
+
+      {/* Tabla */}
+      <FlatList 
+        data={productos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text >{item.nombre}</Text>
+            <Text>${item.precio}</Text>
+          </View>
+        )}
+      />
+
 
     </SafeAreaView>
   )
