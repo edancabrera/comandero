9f0acf29744534b6f4389ec94155000e65fe30c9
edancabrera <ecabrerait@gmail.com>
2025-12-09 12:34:15 -0700

    Adición de las rutas dashboard/articulos y dashboard/mesas

---
+++ src/app/dashboard/articulos.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Articulos = () => {
+  return (
+    <View>
+      <Text>Contenido de Articulos</Text>
+    </View>
+  )
+}
+
+export default Articulos
+
+const styles = StyleSheet.create({})
---
+++ src/app/dashboard/mesas.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Mesas = () => {
+  return (
+    <View>
+      <Text>Contenido de mesas</Text>
+    </View>
+  )
+}
+
+export default Mesas
+
+const styles = StyleSheet.create({})
