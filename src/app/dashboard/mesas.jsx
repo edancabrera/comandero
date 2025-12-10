68c177cc0126c067b8cbfcc2b83d02cb1996d5c0
edancabrera <ecabrerait@gmail.com>
2025-12-10 09:56:58 -0700

    implementación de un SafeAreaView en mesas.jsx para que el contenido no se desborde

--- src/app/dashboard/mesas.jsx
+++ src/app/dashboard/mesas.jsx
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import Areas from '../../components/areas'
 
 const Mesas = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Contenido de mesas</Text>
       <Areas />
-    </View>
+    </SafeAreaView>
   )
 }
 
