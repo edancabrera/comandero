b3c822eedcfb1e48fad5fa6c75622b98b0517a89
edancabrera <ecabrerait@gmail.com>
2025-12-11 11:20:46 -0700

    separación de la pantalla Comandero en tres columnas. Texto con información de la mesa y su area y el botón para cobrar añadidos, únicamente como apoyo visual, aún no tienen funcionalidad

--- src/app/dashboard/comandero.jsx
+++ src/app/dashboard/comandero.jsx
@@ -1,14 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Comandero = () => {
   return (
-    <SafeAreaView>
-      <Text>Pantalla Comandero</Text>
+    <SafeAreaView style={styles.container}>
+
+        {/*Columna izquierda*/}
+        <View style={styles.leftColumnContainer}>
+            <Text>Mesa x Area x</Text>
+            {/*Aqui va la lista de menus*/}
+            <Pressable style={styles.cobrarButton}>
+                <Text>Cobrar Cuenta</Text>
+            </Pressable>
+        </View>
+        
+        {/*Columna central*/}
+        <View style={styles.centralColumnContainer}>
+
+        </View>
+
+        {/*Columna derecha*/}
+        <View style={styles.rightColumnContainer}>
+
+        </View>
+      
     </SafeAreaView>
   )
 }
 
 export default Comandero
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        flexDirection: 'row',
+        backgroundColor: "#2596be"
+    },
+    leftColumnContainer: {
+        flex:0.2,
+        justifyContent: 'space-between'
+    },
+    centralColumnContainer: {
+        flex:0.4,
+        backgroundColor:'red'
+    },
+    rightColumnContainer: {
+        flex:0.4,
+        backgroundColor: 'white'
+    },
+    cobrarButton: {
+        padding:20,
+        borderRadius: 5,
+        alignItems: 'center',
+        backgroundColor: '#faa80f',
+    }
+})
