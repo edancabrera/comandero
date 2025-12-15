d1d51fedc972eb2f85a91b61d230298c972c063f
edancabrera <ecabrerait@gmail.com>
2025-12-15 12:07:03 -0700

    header de la tabla de pedido

--- src/components/comanderoComponents/rightColumnComponents/pedido.jsx
+++ src/components/comanderoComponents/rightColumnComponents/pedido.jsx
@@ -1,13 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View} from "react-native";
 
 const Pedido = () => {
   return (
-    <View>
-      <Text>Tabla Pedido</Text>
+    <View style={styles.tableHeader}>
+      <Text style={styles.tableHeaderText}>Persona</Text>
+      <Text style={styles.tableHeaderText}>Platillo</Text>
+      <Text style={styles.tableHeaderText}>Cantidad</Text>
+      <Text style={styles.tableHeaderText}>Comentarios</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Pedido
+export default Pedido;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  tableHeader: {
+    flexDirection: 'row',
+    paddingVertical: 5,
+    borderBottomWidth: 2,
+    borderColor: "#ffffff88",
+    marginBottom:6,
+  },
+  tableHeaderText: {
+    fontSize: 12,
+    fontWeight: 'bold'
+  },
+});
