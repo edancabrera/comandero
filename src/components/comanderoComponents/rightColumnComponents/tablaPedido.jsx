e38cebf6e63e8304dda25b5bf0cf3f699ef329d3
edancabrera <ecabrerait@gmail.com>
2025-12-15 11:25:14 -0700

    Creación del archivo tablaPedido.jsx

---
+++ src/components/comanderoComponents/rightColumnComponents/tablaPedido.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const tablaPedido = () => {
+  return (
+    <View>
+      <Text>tablaPedido</Text>
+    </View>
+  )
+}
+
+export default tablaPedido
+
+const styles = StyleSheet.create({})
