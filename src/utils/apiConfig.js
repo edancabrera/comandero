c181edbf66a3f1bdf04e3b4478405d137562d187
edancabrera <ecabrerait@gmail.com>
2025-12-29 13:06:51 -0700

    Se creó la carpeta src/utils/ y dentro el archivo apiConfig.js

---
+++ src/utils/apiConfig.js
@@ -0,0 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const IP_KEY = "SERVER_IP";
+
+export const saveServerIp = async (ip) => {
+  await AsyncStorage.setItem(IP_KEY, ip);
+};
+
+export const getServerIp = async () => {
+  return await AsyncStorage.getItem(IP_KEY);
+};
+
+export const buildApiUrl = async (endpoint) => {
+  const ip = await getServerIp();
+  if (!ip) throw new Error("IP no configurada");
+  return `http://${ip}:8080${endpoint}`;
+};
