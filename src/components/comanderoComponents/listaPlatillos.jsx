7e6bb30b03ee35c3c09e6784ba1ad5b42e9c7abd
edancabrera <ecabrerait@gmail.com>
2026-01-02 12:46:44 -0700

    El componente ListaPlatillos realiza una petición a la API, ya no utiliza los datos mock

--- src/components/comanderoComponents/listaPlatillos.jsx
+++ src/components/comanderoComponents/listaPlatillos.jsx
@@ -1,21 +1,45 @@
+import { useEffect, useState } from "react";
 import { useComandero } from "../../context/ComanderoContext";
 
 import { StyleSheet, Text, View, Pressable, ScrollView } from "react-native";
+import { buildApiUrl } from "../../utils/apiConfig";
 
 const ListaPlatillos = () => {
+  const [platillos, setPlatillos] = useState([]);
+
   const { categoriaSeleccionada, agregarPlatillo } = useComandero();
+
+  const obtenerPlatillos = async () => {
+    try {
+      const url = await buildApiUrl(`/platillos/${categoriaSeleccionada?.id}`);
+      const response = await fetch(url);
+      if(!response.ok){
+        throw new Error ('Error en la respuesta  servidor');
+      }
+      const data = await response.json();
+      setPlatillos(data);
+
+    } catch (error) {
+      console.error('Error al obtener los platillos', error)
+    }
+  }
+
+  useEffect(() => {
+    if(!categoriaSeleccionada) return;
+    obtenerPlatillos();
+  }, [categoriaSeleccionada])
 
   return (
     <ScrollView stickyHeaderIndices={[0]}>
-      {categoriaSeleccionada && (<View><Text style={{ backgroundColor: "#2596be", fontWeight: 'bold' }}>Platillos - {categoriaSeleccionada.nombre}</Text></View>)}
+      {categoriaSeleccionada && (<View><Text style={{ backgroundColor: "#2596be", fontWeight: 'bold' }}>Platillos - {categoriaSeleccionada?.nombre}</Text></View>)}
       <View style={styles.container}>
         {categoriaSeleccionada === null ? (
           <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}><Text style={{ color: '#fff', fontWeight: 'bold', fontSize: 18}}>Selecciona una categoría</Text></View>
         ) : (
           <View style={styles.container}>
-            {categoriaSeleccionada.producto.map((platillo) => (
+            {platillos.map((platillo) => (
               <Pressable 
-                key={platillo.idproducto}
+                key={platillo.idProducto}
                 style={styles.platilloButton}
                 onPress={()=> agregarPlatillo(platillo)}
             >
