011f167b5184d7ffd7e11b5cc6e397a331c033d0
edancabrera <ecabrerait@gmail.com>
2026-03-20 14:00:41 -0700

    Implementación de modales de información y acción en modalDividirComanda

--- src/components/modalDividirComanda.jsx
+++ src/components/modalDividirComanda.jsx
@@ -10,6 +10,7 @@ import { useComandero } from "../context/ComanderoContext";
 import { useEffect, useState } from "react";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { buildApiUrl } from "../utils/apiConfig";
+import ModalAccionesDividirComanda from "./modalAccionesDividirComanda";
 
 const ModalDividirComanda = () => {
   const { 
@@ -17,14 +18,20 @@ const ModalDividirComanda = () => {
     setModalDividirComandaVisible,
     mesaSeleccionada, areaSeleccionada,
     obtenerComandaMesa,
-    pedido, setPedido,
     usuario
   } = useComandero();
 
+  const [pedido, setPedido] = useState([]);
   const [nuevoPedido, setNuevoPedido] = useState([]);
   const [comandaActualLineaSeleccionada, setComandaActualLineaSeleccionada] = useState(null);
   const [comandaNuevaLineaSeleccionada, setComandaNuevaLineaSeleccionada] = useState(null);
   const [comandasDivididas, setComandasDivididas] = useState([]);
+
+  const [modalCancelar, setModalCancelar] = useState(false);
+  const [modalImprimir, setModalImprimir] = useState(false);
+  const [modalCuentaDivididaExitosamente, setModalCuentaDivididaExitosamente] = useState(false);
+  const [modalGuardar, setModalGuardar] = useState(false);
+  const [modalCuentasGuardadasExitosamente, setModalCuentasGuardadasExitosamente] = useState(false);
 
   useEffect( () => {
     if(mesaSeleccionada && modalDividirComandaVisible) {
@@ -194,7 +201,7 @@ const ModalDividirComanda = () => {
       if(!response.ok) {
         throw new Error("Error al mandar a cobrar")
       }
-
+      setModalCuentaDivididaExitosamente(true);
       setComandasDivididas(prev => [...prev, [...nuevoPedido]]);
       setNuevoPedido([]);
       setComandaNuevaLineaSeleccionada(null);
@@ -225,6 +232,7 @@ const ModalDividirComanda = () => {
         throw new Error(`Error al guardar comandas divididas de la comanda principal ${idComandaPrincipal}`);
       }
       
+      setModalCuentasGuardadasExitosamente(true);
       setComandasDivididas([]);
     } catch (error) {
       console.error("Error ", error);
@@ -255,6 +263,75 @@ const ModalDividirComanda = () => {
       visible={modalDividirComandaVisible}
       //onRequestClose={() => setModalDividirComandaVisible(false)}
     >
+      <ModalAccionesDividirComanda 
+        title="Advertencia"
+        paragraph="Se perderán todas las modificaciones, ¿Aún así desea cancelar?"
+        action={() => setModalDividirComandaVisible(false)}
+        visiblity={modalCancelar}
+        setVisiblity={setModalCancelar}
+        alert = {true}
+      />
+
+      <ModalAccionesDividirComanda 
+        title={nuevoPedido.length !== 0 ? "Confirmar" : "Error"}
+        paragraph={nuevoPedido.length !== 0 ? "¿Está seguro de las modificaciones?" : "Debe agregar al menos un producto a la nueva comanda"}
+        action={() => { if (nuevoPedido.length !== 0) { imprimirNuevaComanda()}}}
+        visiblity={modalImprimir}
+        setVisiblity={setModalImprimir}
+        infoOnlyModal={nuevoPedido.length !== 0 ? false : true}
+        alert={nuevoPedido.length === 0}
+        question={nuevoPedido.length !== 0}
+      />
+
+      <ModalAccionesDividirComanda 
+        title={"Cuenta impresa"}
+        paragraph={"Cuenta separada correctamente"}
+        visiblity={modalCuentaDivididaExitosamente}
+        setVisiblity={setModalCuentaDivididaExitosamente}
+        infoOnlyModal={true}
+        successfull={true}
+        titleColor="green"
+      />
+
+      <ModalAccionesDividirComanda 
+        title={( 
+          pedido.length !== 0 || nuevoPedido.length !== 0 || comandasDivididas.length === 0)
+            ? "Error"
+            : "Confirmar"
+          }
+        paragraph={
+          pedido.length !== 0
+            ? "Debe modificar completamente la comanda original para guardar"
+            : nuevoPedido.length !== 0
+            ? "Debe mandar a imprimir todas las comandas nuevas para guardar"
+            : comandasDivididas.length === 0
+            ? "No hay comandas nuevas que guardar"
+            : "¿Desea guardar las comandas separadas?"
+        }
+        action={() => {
+          if (pedido.length === 0 && nuevoPedido.length === 0) {
+            guardarComandasDivididas();
+          } 
+        }}
+        visiblity={modalGuardar}
+        setVisiblity={setModalGuardar}
+        infoOnlyModal={pedido.length !== 0 || nuevoPedido.length !== 0 || comandasDivididas.length === 0}
+        alert={pedido.length !== 0 || nuevoPedido.length !== 0 || comandasDivididas.length === 0}
+        question={pedido.length === 0 && nuevoPedido.length === 0 && comandasDivididas.length !== 0}
+      />
+
+      <ModalAccionesDividirComanda 
+        title={"Cuenta dividida"}
+        paragraph={"Cuenta separada correctamente"}
+        action={() => setModalDividirComandaVisible(false)}
+        visiblity={modalCuentasGuardadasExitosamente}
+        setVisiblity={setModalCuentasGuardadasExitosamente}
+        infoOnlyModal={true}
+        infoAndActionModal={true}
+        successfull={true}
+        titleColor="green"
+      />
+
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
           <Text style ={styles.title}>Dividir Comanda - {mesaSeleccionada?.nombre}</Text>
@@ -364,7 +441,7 @@ const ModalDividirComanda = () => {
                 pressed && styles.buttonPressed
               ]}
               onPress={() => {
-                setModalDividirComandaVisible(false);
+                setModalCancelar(true);
               }}
             >
               <Text>Cancelar</Text>
@@ -375,17 +452,7 @@ const ModalDividirComanda = () => {
                 styles.button,
                 pressed && styles.buttonPressed
               ]}
-              onPress={() => {
-                if(pedido.length !== 0 || nuevoPedido.length !== 0) {
-                  console.log('No deben exisitr platillos en ningua de las tablas');
-                  return
-                };
-                if(comandasDivididas.length === 0){
-                  console.log('No hay comandas nuevas que guardar');
-                  return
-                }
-                guardarComandasDivididas();
-              }}
+              onPress={() => { setModalGuardar(true) }}
             >
               <Text>Guardar</Text>
             </Pressable>
@@ -395,7 +462,7 @@ const ModalDividirComanda = () => {
                 styles.button,
                 pressed && styles.buttonPressed
               ]}
-              onPress={imprimirNuevaComanda}
+              onPress={() =>{ setModalImprimir(true) }}
             >
               <Text>Imprimir</Text>
             </Pressable>
@@ -409,111 +476,4 @@ const ModalDividirComanda = () => {
 
 export default ModalDividirComanda;
 
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0,0,0,0.5)",
-  },
-  modalView: {
-    width: "98%",
-    height: "90%",
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 10,
-    padding: 15,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  title:{
-    backgroundColor: '#faa80f',
-    textAlign:'center',
-    fontWeight: 'bold',
-    fontSize: 16
-  },
-  tablesContainer: {
-    flexDirection: "row",
-    flex: 1,
-    gap: 5
-  },
-  tableTitle:{
-    backgroundColor: '#fff',
-    textAlign:'center',
-    fontWeight: 'bold',
-    fontSize: 10
-  },
-  table: {
-    flex: 1,
-    backgroundColor: "#f5f5f5",
-    borderRadius: 10,
-    padding: 10,
-  },
-  tableHeader: {
-    flexDirection: "row",
-    borderBottomWidth: 2,
-    borderColor: "#00000088",
-    paddingVertical: 5,
-    paddingHorizontal: 5,
-    backgroundColor: "#fff",
-    alignContent: "space-evenly",
-  },
-  tableRow:{
-    flexDirection: 'row',
-    paddingVertical: 4,
-    borderBottomWidth: .5,
-    borderColor: "#00000088",
-  },
-  rowSelected: {
-    backgroundColor: "#e0f2ff"
-  },
-  colPersona: {
-    flex: 1,
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-  colPlatillo: {
-    flex: 2,
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-  colCantidad: {
-    flex: 1,
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-  actionButtonsContainer:{
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 5
-  },
-  actionButton:{
-    backgroundColor: "#f5f5f5",
-    padding: 5,
-    borderRadius: 6,
-    margin: 5,
-    alignItems: "center",
-    width: 40,
-  },
-  buttonsContainer:{
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  button: {
-      backgroundColor: "#f5f5f5",
-      padding: 20,
-      borderRadius: 5,
-      margin: 5,
-      alignItems: "center",
-      width: 145,
-    },
-  buttonPressed: {
-    backgroundColor: "#e0e0e0"
-  }
-});
+const styles = StyleSheet.create({ });
